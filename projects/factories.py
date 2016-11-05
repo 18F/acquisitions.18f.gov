6647999f7d085cc5d1162d4b7d082b5d13221605
stvnrlly <steven.reilly@gsa.gov>
2016-11-04 21:35:08 -0400

    update projects factory

--- projects/factories.py
+++ projects/factories.py
@@ -1,4 +1,6 @@
 import factory
+import string
+import factory.fuzzy
 from projects.models import IAA, Project
 
 
@@ -6,7 +8,10 @@ class IAAFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = IAA
 
-    id = factory.Faker('md5')
+    id = factory.fuzzy.FuzzyText(
+        prefix='IAA',
+        chars=string.digits,
+        )
     client = factory.Faker('company')
 
 
@@ -17,3 +22,4 @@ class ProjectFactory(factory.django.DjangoModelFactory):
     iaa = factory.SubFactory(IAAFactory)
     description = factory.Faker('paragraphs')
     name = factory.Faker('catch_phrase')
+    public = factory.Faker('boolean')
