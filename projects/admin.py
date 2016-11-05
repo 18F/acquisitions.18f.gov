a9c089cba6787190f6ea8ab1abdb6bea23822798
stvnrlly <steven.reilly@gsa.gov>
2016-11-05 14:44:32 -0400

    move admin registration to proper app

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from projects.models import IAA, Project
+
 
 # Register your models here.
+@admin.register(IAA, Project)
+class ProjectAdmin(admin.ModelAdmin):
+    pass
