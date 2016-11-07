0ee3d57351be59b040259b2153cbe56623bfd89e
stvnrlly <steven.reilly@gsa.gov>
2016-11-07 09:14:22 -0500

    add missing comma

--- team/urls.py
+++ team/urls.py
@@ -5,9 +5,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^api/people/$', views.TeammateList.as_view()),
-    url(r'^api/people/(?P<pk>[0-9]+)', views.TeammateDetail.as_view())
+    url(r'^api/people/(?P<pk>[0-9]+)', views.TeammateDetail.as_view()),
     url(r'^api/roles/$', views.RoleList.as_view()),
-    url(r'^api/roles/(?P<pk>[0-9]+)', views.RoleDetail.as_view())
+    url(r'^api/roles/(?P<pk>[0-9]+)', views.RoleDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
