bf4f0424013ad955230d6cad456626fc14e77783
stvnrlly <steven.reilly@gsa.gov>
2016-11-06 22:03:57 -0500

    class-based views

--- projects/urls.py
+++ projects/urls.py
@@ -1,18 +1,12 @@
 from django.conf.urls import include, url
 from projects import views
-from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
 
-# router = routers.DefaultRouter()
-# router.register(r'iaa', views.IAAViewSet, base_name='iaa')
-# router.register(r'projects', views.ProjectViewSet, base_name='projects')
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    # url(r'^api/', include(router.urls)),
-    # url(r'^api/projects/$', views.ProjectList.as_view())
-    url(r'^api/projects/$', views.project_list),
-    url(r'^api/projects/(?P<pk>[0-9]+)$', views.project_detail)
+    url(r'^api/projects/$', views.ProjectList.as_view()),
+    url(r'^api/projects/(?P<pk>[0-9]+)$', views.ProjectDetail.as_view())
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
 from rest_framework import status
@@ -28,12 +28,12 @@ def home(request):
 #             return IAA.objects.exclude(signed_on=null)
 
 
-@api_view(['GET'])
-def project_list(request, format=None):
-    if request.method == 'GET':
-        projects = Project.objects.all()
-        serializer = ProjectSerializer(projects, many=True)
-        return Response(serializer.data)
+# @api_view(['GET'])
+# def project_list(request, format=None):
+#     if request.method == 'GET':
+#         projects = Project.objects.all()
+#         serializer = ProjectSerializer(projects, many=True)
+#         return Response(serializer.data)
 
 
 @api_view(['GET'])
@@ -48,18 +48,36 @@ def project_detail(request, pk, format=None):
         return Response(serializer.data)
 
 
-# class ProjectList(APIView):
-#     """
-#     List all projects
-#     """
-#
-#
-#     def get_queryset(self):
-#         if self.request.user.has_perm('projects.view_private'):
-#             return Project.objects.all()
-#         else:
-#             return Project.objects.filter(public=True)
-#
-#     def get(self, request, *args, **kwargs):
-#         serializer = ProjectSerializer
-#         return JSONResponse(serializer.data)
+class ProjectList(APIView):
+    """
+    List all projects
+    """
+    def get_queryset(self):
+        if self.request.user.has_perm('projects.view_private'):
+            return Project.objects.all()
+        else:
+            return Project.objects.filter(public=True)
+
+    def get(self, request, format=None):
+        queryset = self.get_queryset()
+        serializer = ProjectSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class ProjectDetail(APIView):
+    """
+    Retrieve details of one project
+    """
+    def get_object(self, pk):
+        project = Project.objects.get(pk=pk)
+        if project.public:
+            return project
+        elif self.request.user.has_perm('projects.view_private'):
+            return project
+        else:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        queryset = self.get_object(pk)
+        serializer = ProjectSerializer(queryset)
+        return Response(serializer.data)
