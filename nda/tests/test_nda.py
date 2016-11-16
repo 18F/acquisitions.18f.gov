790897bc4931b906d9b7ac3a7b28c2aa40f45dec
stvnrlly <steven.reilly@gsa.gov>
2016-11-15 21:25:51 -0500

    test nda form flow with unittest style

--- nda/tests/test_nda.py
+++ nda/tests/test_nda.py
@@ -1,5 +1,6 @@
 import pytest
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import User, Group
+from django.test import TestCase, Client
 from nda.views import sign_nda
 from web.views import profile
 
@@ -44,3 +45,20 @@ class TestNDA:
         request.user = user
         response = sign_nda(request)
         assert "previously signed the NDA" in str(response.content)
+
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.group = Group.objects.create(name='NDA Signed')
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username='test', email='test@wow.biz', password='top_secret')
+
+    def test_need_nda(self):
+        self.client.force_login(self.user)
+        response = self.client.get('/profile/sign_nda/')
+        self.assertTemplateUsed(response, 'nda/nda.html')
+        response = self.client.post('/profile/sign_nda/', {'agree': True})
+        self.assertTemplateUsed(response, 'nda/success.html')
+        response = self.client.get('/profile/sign_nda/')
+        self.assertTemplateUsed(response, 'nda/already-signed.html')
