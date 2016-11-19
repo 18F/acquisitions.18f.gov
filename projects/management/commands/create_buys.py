785fc6e0c1d8c90a5734586b630d19a22d016212
stvnrlly <steven.reilly@gsa.gov>
2016-11-18 23:09:52 -0500

    create_buys allows any number

--- projects/management/commands/create_buys.py
+++ projects/management/commands/create_buys.py
@@ -13,9 +13,17 @@ class Command(BaseCommand):
             default=False,
             help='Add to existing projects instead of creating new ones'
         )
+        parser.add_argument(
+            '-n', '--number',
+            action='store',
+            default=5,
+            help='The number of buys to create'
+        )
 
     def handle(self, *args, **options):
+        number = int(options['number'])
+
         if options['add']:
-            AddBuyFactory.create_batch(5)
+            AddBuyFactory.create_batch(number)
         else:
-            BuyFactory.create_batch(5)
+            BuyFactory.create_batch(number)
