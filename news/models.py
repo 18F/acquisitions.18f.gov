67249461186781a53d67c912c9a5ab09c190e7e9
stvnrlly <steven.reilly@gsa.gov>
2016-11-25 15:14:14 -0500

    add news model

--- news/models.py
+++ news/models.py
@@ -1,3 +1,43 @@
+from datetime import datetime
 from django.db import models
+from django.core.exceptions import ValidationError
+
 
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(
+        max_length=100,
+        blank=False,
+        null=False,
+    )
+    update = models.TextField(
+        blank=False,
+        null=False,
+    )
+    publication_date = models.DateTimeField(
+        blank=True,
+        null=True,
+    )
+    draft = models.BooleanField(
+        default=True,
+    )
+
+    def __str__(self):
+        return "{0} | {1}".format(self.title, self.publication_date)
+
+    def clean(self):
+        if self.draft and self.publication_date is not None:
+            raise ValidationError({
+                'publication_date': 'Drafts may not have a publication date'
+            })
+        if not self.draft and self.publication_date is None:
+            raise ValidationError({
+                'publication_date': 'Please set a publication date'
+            })
+        if self.publication_date < datetime.now():
+            raise ValidationError({
+                'publication_date': 'Publication date may not be in the past'
+            })
+
+    class Meta:
+        pass
