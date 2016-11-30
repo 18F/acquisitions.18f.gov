8896dc9f10f6cb8cca377b31d86d00d9ecc8f565
stvnrlly <steven.reilly@gsa.gov>
2016-11-30 00:48:23 -0500

    actually add new userfactory to git

---
+++ acquisitions/factories.py
@@ -0,0 +1,15 @@
+import factory
+from django.contrib.auth.models import User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Faker('user_name')
+    email = factory.Faker('safe_email')
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    is_active = True
+    is_staff = False
+    is_superuser = False
