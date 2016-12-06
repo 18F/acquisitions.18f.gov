36b4f97306a97b08765d277f2f9e7bcb444405df
stvnrlly <steven.reilly@gsa.gov>
2016-12-05 22:00:14 -0500

    add test

---
+++ nda/tests/test_buy_nda.py
@@ -0,0 +1,27 @@
+import pytest
+from django.contrib.auth.models import User, Group
+from projects.factories import BuyFactory
+from acquisitions.factories import UserFactory
+
+
+class TestBuyNDA:
+    @pytest.fixture
+    @pytest.mark.django_db
+    def user(self):
+        user = UserFactory()
+        group = Group.objects.create(name='NDA Signed')
+        user.groups.add(group)
+        return user
+
+    @pytest.fixture
+    @pytest.mark.django_db
+    def buy(self):
+        buy = BuyFactory()
+        return buy
+
+    @pytest.mark.django_db
+    def test_signing_nda(self, user, buy):
+        buy.technical_evaluation_panel.add(user)
+        assert buy.all_nda_signed() is False
+        buy.nda_signed.add(user)
+        assert buy.all_nda_signed() is True
