25ecce316242882d8cba740025923c33b66d44c6
stvnrlly <steven.reilly@gsa.gov>
2016-12-06 14:09:06 -0500

    simplify filtersets and add foreign keys back in

--- projects/filters.py
+++ projects/filters.py
@@ -2,28 +2,13 @@ import django_filters
 from projects.models import IAA, Project, Buy
 
 
-class WorkFilter(django_filters.rest_framework.FilterSet):
-    min_dollars = django_filters.NumberFilter(
-        name="dollars",
-        lookup_expr='gte',
-    )
-    max_dollars = django_filters.NumberFilter(
-        name="dollars",
-        lookup_expr='lte',
-    )
-
-    class Meta:
-        # Allowing filtering by project leaks non-public project names in the
-        # browseable API, so it cannot be provided here until the browseable
-        # API is turned off
-        fields = ['id', 'name', 'min_dollars', 'max_dollars']
-
-
-class BuyFilter(WorkFilter):
+class BuyFilter(django_filters.rest_framework.FilterSet):
     class Meta:
         model = Buy
+        fields = ['id', 'name', 'project__id']
 
 
-class ProjectFilter(WorkFilter):
+class ProjectFilter(django_filters.rest_framework.FilterSet):
     class Meta:
         model = Project
+        fields = ['id', 'name', 'iaa__id']
