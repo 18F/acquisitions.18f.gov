05a90bcb747e834f61b80bd8d3532348f1984fd1
stvnrlly <steven.reilly@gsa.gov>
2016-12-05 22:57:00 -0500

    update test with new nda content

--- nda/tests/test_markdown.py
+++ nda/tests/test_markdown.py
@@ -4,4 +4,7 @@ from django.template.loader import render_to_string
 
 def test_markdown():
     rendered = render_to_string('nda/nda.html')
-    assert "<h1>Non-Disclosure Agreement</h1>" in rendered
+    # The h1 tag has a tab due to markdown rendering for some reason
+    expected = "<h1>    Non-Disclosure Agreement for TTS Office of "\
+               "Acquisitions</h1>"
+    assert expected in rendered
