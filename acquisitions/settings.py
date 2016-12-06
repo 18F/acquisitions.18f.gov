c3d7d25b4b6c05d0d696bcd385fe8593203a9dbd
stvnrlly <steven.reilly@gsa.gov>
2016-12-06 12:06:39 -0500

    remove browseable api

--- acquisitions/settings.py
+++ acquisitions/settings.py
@@ -166,7 +166,6 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_RENDERER_CLASSES': (
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework_csv.renderers.CSVRenderer',
     ),
     'DEFAULT_FILTER_BACKENDS': (
