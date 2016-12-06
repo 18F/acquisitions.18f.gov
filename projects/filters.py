db6f47d4010434d3d98a76fe13e3f5c8899146aa
stvnrlly <steven.reilly@gsa.gov>
2016-12-06 14:20:22 -0500

    slightly nicer query param

--- projects/filters.py
+++ projects/filters.py
@@ -3,12 +3,16 @@ from projects.models import IAA, Project, Buy
 
 
 class BuyFilter(django_filters.rest_framework.FilterSet):
+    project_id = django_filters.CharFilter(name="project__id")
+
     class Meta:
         model = Buy
-        fields = ['id', 'name', 'project__id']
+        fields = ['id', 'name', 'project_id']
 
 
 class ProjectFilter(django_filters.rest_framework.FilterSet):
+    iaa_id = django_filters.CharFilter(name="iaa__id")
+
     class Meta:
         model = Project
-        fields = ['id', 'name', 'iaa__id']
+        fields = ['id', 'name', 'iaa_id']
