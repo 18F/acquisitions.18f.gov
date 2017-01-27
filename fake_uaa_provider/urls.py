094968155bdaf326a4b6210d672705077c9bae08
stvnrlly <steven.reilly@gsa.gov>
2017-01-27 07:12:35 -0500

    move uaa_client to new package

--- fake_uaa_provider/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^oauth/authorize$', views.authorize, name='auth'),
-    url(r'^oauth/token$', views.access_token, name='token'),
-]
--- uaa_client/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^callback$', views.oauth2_callback, name='callback'),
-    url(r'^login$', views.login, name='login'),
-    url(r'^logout$', views.logout, name='logout'),
-]
